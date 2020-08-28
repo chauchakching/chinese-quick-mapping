8d3f777b48614ed01618cb8ad82e12c140fc2df6
chauchakching <chauchakching@gmail.com>
2020-08-28 14:21:04 +0800

    fix service worker url on github pages case

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const {Elm} = require('./Main.elm')
 // register service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    // use a relative path here to make it work of github pages with base pathname
+    // might not work if visit non home page
+    navigator.serviceWorker.register('service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
